4a3bb0908bf52602811288f5f53911071c6d2ecb
Marianna-Bogutska <kuschak16@gmail.com>
2021-06-25 08:25:36 +0000

    add js

---
+++ css/js/carousel.js
@@ -0,0 +1,18 @@
+const images = [
+    '../img/september.webp',
+    '../img/march.webp',
+    '../img/may2021.webp',
+];
+let currentSlide = 0;
+function showCurrentSlide() {
+    const imageContainer = document.querySelector('.products-carousel .product-image');
+    imageContainer.src = images[currentSlide];
+}
+function next() {
+    currentSlide ++;
+    if (currentSlide > images.length - 1) currentSlide = 0;
+    showCurrentSlide();
+}
+    setInterval(next, 1000);
+    showCurrentSlide();
+
